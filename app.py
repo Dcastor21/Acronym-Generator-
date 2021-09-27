3eb7b724fcd0986869e4e41a922ab0a8a0ff8872
Darnel Castor <darnelcastor2@gmail.com>
2021-09-27 12:07:32 -0400

    adding comments to script

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+#get the user input and convert it into a troy
 user_input = str(input("Enter a Phrase: "))
+#split the user input 
 text = user_input.split()
+#declare a new varirable to store the acronym 
 a = " "
+
 for i in text:
     a = a+str(i[0]).upper()
 print(a)
